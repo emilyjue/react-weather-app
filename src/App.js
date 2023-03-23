61baa66adf1e6132fd0087f14bdde2b8b1aeb2bf
emilyjue <27742336+emilyjue@users.noreply.github.com>
2023-03-23 16:08:14 -0500

    Divide css files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Search from "./Search";
-import "./style.css";
+import "./App.css";
 
 export default function App() {
   return (
--- src/Search.js
+++ src/Search.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import WeatherInfo from "./WeatherInfo";
+import "./Search.css";
 
 export default function Search() {
   let [search, setSearch] = useState("");
